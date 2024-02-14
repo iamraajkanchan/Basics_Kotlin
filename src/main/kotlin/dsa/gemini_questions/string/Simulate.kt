ce994e9bd413edcaa7d61017806dfc4c77e879ce
abhishek7738127700 <abhishektiwaribecse@gmail.com>
2024-02-14 22:29:03 +0530

    Working on MakeThreeStringsEqual.kt class.

--- src/main/kotlin/dsa/gemini_questions/string/Simulate.kt
+++ src/main/kotlin/dsa/gemini_questions/string/Simulate.kt
@@ -1,9 +1,10 @@
 package dsa.gemini_questions.string
 
+import dsa.gemini_questions.string.easy.MakeThreeStringsEqual
 import dsa.gemini_questions.string.easy.Palindrome
 import dsa.gemini_questions.string.easy.ReverseString
 import dsa.gemini_questions.string.easy.RomanToInteger
 
 fun main(args: Array<String>) {
-    RomanToInteger.testRomanToInteger()
+    MakeThreeStringsEqual.testMakeThreeStringsEqual()
 }
--- src/main/kotlin/dsa/gemini_questions/string/easy/MakeThreeStringsEqual.kt
+++ src/main/kotlin/dsa/gemini_questions/string/easy/MakeThreeStringsEqual.kt
@@ -3,8 +3,16 @@ package dsa.gemini_questions.string.easy
 import jetbrains_academy.getLength
 
 class MakeThreeStringsEqual {
-    private fun findMinimumOperations(firstString: String, secondString: String, thirdString: String) : Int {
-        val inputLengths = listOf(firstString, secondString, thirdString).map { e -> e.length }.toList()
+    private fun findMinimumOperations(firstString: String, secondString: String, thirdString: String): Int {
+        val inputLengths = listOf(firstString, secondString, thirdString).map(String::length).toList()
+        println(inputLengths)
         return -1
+    }
+
+    companion object {
+        fun testMakeThreeStringsEqual() {
+            val makeThreeStringsEqual = MakeThreeStringsEqual()
+            makeThreeStringsEqual.findMinimumOperations("abc", "abd", "ab")
+        }
     }
 }
