1321b7f2633862aa4349a6ad2232564b8186284c
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-06-27 12:45:41 +0530

    Learning about channel. Probably!!!

--- src/main/kotlin/coroutines/BasicCoroutines.kt
+++ src/main/kotlin/coroutines/BasicCoroutines.kt
@@ -1,10 +1,19 @@
 package coroutines
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class BasicCoroutines {
-    suspend fun getValueAfterThreeSeconds() : String {
-        delay(3000)
-        return "Raj, you are late by 3 seconds"
+    suspend fun getValueAfterThreeSeconds() : ReceiveChannel<String> = coroutineScope {
+        val channel = Channel<String>()
+        launch {
+            delay(3000)
+            channel.send("Raj, you are late by 3 seconds")
+            channel.close()
+        }
+        channel
     }
 }
