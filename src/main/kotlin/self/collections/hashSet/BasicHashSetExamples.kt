546553ca939815ca3d00ad073dd0372eadc9501a
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 18:09:11 +0530

    Description and Example is added in BasicHashSetExamples.kt

--- src/main/kotlin/self/collections/hashSet/BasicHashSetExamples.kt
+++ src/main/kotlin/self/collections/hashSet/BasicHashSetExamples.kt
@@ -1,4 +1,31 @@
 package self.collections.hashSet
 
+/**
+ * Quality: Unique Values
+ * When to use: Store unique values.
+ * Scenario: A QR ticket should not be scanned twice. Duplicates should be ignored.
+ * */
 class BasicHashSetExamples {
+
+    private val stringSet = HashSet<String>()
+
+    fun addElementOperation(element: String) {
+        stringSet.add(element)
+    }
+
+    fun removeElementOperation(element: String){
+        stringSet.remove(element)
+    }
+
+    /*
+    * This operation is not available for a Set
+    * */
+
+    @Deprecated("This operation doesn't work on a set!")
+    fun removeElementByIndexOperation(index: Int) {
+
+    }
+
+
+    fun getHashSet(): HashSet<String> = stringSet
 }
