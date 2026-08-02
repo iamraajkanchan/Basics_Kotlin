ac87d0d763f097232706576d65c7cb95417369a4
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 22:33:04 +0530

    Description is added for BasicLinkedListExamples.kt

--- src/main/kotlin/self/collections/linkedList/BasicLinkedListExamples.kt
+++ src/main/kotlin/self/collections/linkedList/BasicLinkedListExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.linkedList
 
+/**
+ * Quality: Insertion / Deletion
+ * When to use: Frequent insertion/removal in the middle or at the beginning.
+ * Scenario: Passengers are standing in a queue. VIP arrives at the front.
+ * Why use it?: Efficient insertion/removal without shifting elements. Not good for random access.
+ * */
 class BasicLinkedListExamples {
 }
