265bbb8764f27e2c340a65ecae05de8298604980
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 23:40:08 +0530

    Description is added for BasicMutableSetExamples.kt

--- src/main/kotlin/self/collections/mutableSet/BasicMutableSetExamples.kt
+++ src/main/kotlin/self/collections/mutableSet/BasicMutableSetExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.mutableSet
 
+/**
+ * Quality: Unique & Dynamic
+ * When to use: Unique items that can change.
+ * Scenario: Employees who entered today.
+ * Why use it?: Always keeps only unique values.
+ * */
 class BasicMutableSetExamples {
 }
