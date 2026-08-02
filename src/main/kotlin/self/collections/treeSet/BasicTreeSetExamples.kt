2e57cba54c0dcd798d44a0770d66823887302fe2
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 23:40:45 +0530

    Description is added for BasicTreeSetExamples.kt

--- src/main/kotlin/self/collections/treeSet/BasicTreeSetExamples.kt
+++ src/main/kotlin/self/collections/treeSet/BasicTreeSetExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.treeSet
 
+/**
+ * Quality: Unique + Sorted
+ * When to use: Automatically sort unique items.
+ * Scenario: Train numbers should be unique and sorted.
+ * Why use it?: Duplicates removed and elements are sorted.
+ * */
 class BasicTreeSetExamples {
 }
