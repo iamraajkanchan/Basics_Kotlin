fe242cf0876aa1640640d7d99b4321a37d735823
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 18:08:52 +0530

    Description and Example is added in BasicHashMapExamples.kt

--- src/main/kotlin/self/collections/hashMap/BasicHashMapExamples.kt
+++ src/main/kotlin/self/collections/hashMap/BasicHashMapExamples.kt
@@ -1,4 +1,27 @@
 package self.collections.hashMap
 
+/**
+ * Quality: Key -> Value
+ * When to use: Lookup by a unique key.
+ * Scenario: Find passenger name using Ticket ID:
+ * Why use it?: Very fast key-value lookup.
+ * */
+
 class BasicHashMapExamples {
+
+    private val stringHashMap = HashMap<Int, String>()
+
+    fun addElementOperation(index: Int, element: String) {
+        stringHashMap[index] = element
+    }
+
+    fun removeElementByKeyOperation(index: Int) {
+        stringHashMap.remove(index)
+    }
+
+    fun removeElementByKeyAndValueOperation(oldIndex: Int, newElement: String) {
+        stringHashMap.remove(oldIndex, newElement)
+    }
+
+    fun getHashMap(): HashMap<Int, String> = stringHashMap
 }
