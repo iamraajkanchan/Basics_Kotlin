57f92e301a023dd13da46dde3ad39e4d8d82d558
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 23:40:31 +0530

    Description is added for BasicTreeMapExamples.kt

--- src/main/kotlin/self/collections/treeMap/BasicTreeMapExamples.kt
+++ src/main/kotlin/self/collections/treeMap/BasicTreeMapExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.treeMap
 
+/**
+ * Quality: Sorted Keys
+ * When to use: Key should remain sorted.
+ * Scenario: Stations by station code should be in order.
+ * Why use it?: Keys are sorted automatically.
+ * */
 class BasicTreeMapExamples {
 }
