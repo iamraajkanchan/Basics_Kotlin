6dce76fd30bc7be5630587b4e2cd3fc15e41f230
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 23:39:38 +0530

    Description is added for BasicMutableMapExamples.kt

--- src/main/kotlin/self/collections/mutableMap/BasicMutableMapExamples.kt
+++ src/main/kotlin/self/collections/mutableMap/BasicMutableMapExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.mutableMap
 
+/**
+ * Quality: Key-Value & Dynamic.
+ * When to use: Frequently update key-value pairs.
+ * Scenario: Metro card balance changes are recharge.
+ * Why use it?: Easy to update values for existing keys.
+ * */
 class BasicMutableMapExamples {
 }
