ad8b899c5de7dcbda0007a7c78f927eb8bebd21f
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 22:32:34 +0530

    Description is added for BasicLinkedHashMapExamples.kt

--- src/main/kotlin/self/collections/linkedHashMap/BasicLinkedHashMapExamples.kt
+++ src/main/kotlin/self/collections/linkedHashMap/BasicLinkedHashMapExamples.kt
@@ -1,4 +1,12 @@
 package self.collections.linkedHashMap
 
+
+/**
+ * Quality: (Key -> Value) With Order
+ * When to use: Key - value lookup while preserving insertion order
+ * Scenario: Transaction log where order of events matters
+ * Why use it? Maintains insertion order of key-value pairs
+ * */
 class BasicLinkedHashMapExamples {
+
 }
