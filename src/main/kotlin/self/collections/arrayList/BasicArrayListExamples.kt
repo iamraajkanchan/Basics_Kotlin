aac302c46439ff6d316cdc1900a42c9f5c316489
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 18:08:33 +0530

    Description and Example is added in BasicArrayListExamples.kt

--- src/main/kotlin/self/collections/arrayList/BasicArrayListExamples.kt
+++ src/main/kotlin/self/collections/arrayList/BasicArrayListExamples.kt
@@ -1,4 +1,28 @@
 package self.collections.arrayList
 
+
+/**
+ * Quality: Dynamic List
+ * When to use: Frequently add items and access them by index.
+ * Scenario: Passengers keep entering the station.
+ * Why use it?: Size changes easy to add items and fast random access.
+ * */
 class BasicArrayListExamples {
+
+    private val stringList = ArrayList<String>()
+
+    fun addElementOperation(element: String) {
+        stringList.add(element)
+    }
+
+    fun removeElementOperation(element: String) {
+        stringList.remove(element)
+    }
+
+    fun removeElementByIndex(index: Int) {
+        stringList.removeAt(index)
+    }
+
+    fun getArrayList() : ArrayList<String> = stringList
+
 }
