00a8385569b37d7864c0855bbfa743e157904309
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 23:39:14 +0530

    Description is added for BasicMutableListExamples.kt

--- src/main/kotlin/self/collections/mutableList/BasicMutableListExamples.kt
+++ src/main/kotlin/self/collections/mutableList/BasicMutableListExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.mutableList
 
+/**
+ * Quality: Dynamic Ordered List.
+ * When to use: Ordered data where duplicates are allowed and updates are common.
+ * Scenario: Today's train schedule may change.
+ * Why use it?: Easy to add, remove and update items.
+ * */
 class BasicMutableListExamples {
 }
