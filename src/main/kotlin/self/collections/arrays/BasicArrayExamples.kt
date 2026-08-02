57ea6ebb93e17962569d945ab49a4edf35c0417a
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 22:32:16 +0530

    Description is added for BasicArrayExamples.kt

--- src/main/kotlin/self/collections/arrays/BasicArrayExamples.kt
+++ src/main/kotlin/self/collections/arrays/BasicArrayExamples.kt
@@ -1,4 +1,10 @@
 package self.collections.arrays
 
+/**
+ * Quality: Fixed Size
+ * When to use: Fixed-size data. The number of elements is known beforehand.
+ * Scenario: A metro station has exactly 4 gates.
+ * Why use it?: Fast access by index and less memory overhead.
+ * */
 class BasicArrayExamples {
 }
