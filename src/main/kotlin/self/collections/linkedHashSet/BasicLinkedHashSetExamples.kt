4f1ea76c9e8246fa708b2d8cbc5a1c6bfcc58097
Raaj Kanchan <iamraajkanchan@gmail.com>
2026-08-02 22:32:48 +0530

    Description is added for BasicLinkedHashSetExamples.kt

--- src/main/kotlin/self/collections/linkedHashSet/BasicLinkedHashSetExamples.kt
+++ src/main/kotlin/self/collections/linkedHashSet/BasicLinkedHashSetExamples.kt
@@ -1,4 +1,11 @@
 package self.collections.linkedHashSet
 
+/**
+ * Quality: Unique + Order
+ * When to use: Unique value and also preserve insertion order.
+ * Scenario: Remember the order in which passengers entered the station.
+ * Why use it?: Maintains insertion order with unique values.
+ *
+ * */
 class BasicLinkedHashSetExamples {
 }
